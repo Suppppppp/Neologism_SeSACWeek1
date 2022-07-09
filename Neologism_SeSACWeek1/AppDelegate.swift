3c3c5608855961fd7cd18b010eca5aedae0faedd
Suppppppp <u1316@daum.net>
2022-07-09 18:47:05 +0900

    add Image,  icon source , LaunchScreen

--- Neologism_SeSACWeek1/AppDelegate.swift
+++ Neologism_SeSACWeek1/AppDelegate.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 @main
+@available(iOS 13.0, *)
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        sleep(3)
         return true
     }
 
