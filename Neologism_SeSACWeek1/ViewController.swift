8846a2d2e3ae14d0aa0828f23455494c9077387c
Suppppppp <u1316@daum.net>
2022-07-09 16:54:20 +0900

    Initial Commit

---
+++ Neologism_SeSACWeek1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neologism_SeSACWeek1
+//
+//  Created by KYUNGSUP GO on 2022/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
